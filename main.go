8a3a2e85f70522dbc9827bab1a88332a72a2d138
Devan Goodwin <dgoodwin@redhat.com>
2016-08-18 10:05:17 -0300

    Ensure CA is available for serving on startup.

--- main.go
+++ main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
+
+// CAPath is the expected location of our cluster's CA to be distributed to
+// clients looking to connect.
+const CAPath = "/var/lib/kubernetes/ca.pem"
 
 func main() {
 
-	router := NewRouter()
+	// Make sure the CA cert for the cluster exists and is readable:
+	if _, err := os.Stat(CAPath); os.IsNotExist(err) {
+		log.Fatalf("CA does not exist: %s", CAPath)
+	}
+	// Test read permissions
+	file, err := os.Open(CAPath)
+	if err != nil {
+		log.Fatalf("ERROR: Unable to read %s", CAPath)
+	}
+	file.Close()
 
+	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
